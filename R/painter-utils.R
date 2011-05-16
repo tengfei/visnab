522be8898a11cd572ca0827f7d85ecf5c28753e6
tengfei <tengfei@tengfei-T510.(none)>
2011-05-16 13:24:06 -0700

    add own defined pal() for fast mapping

--- R/painter-utils.R
+++ R/painter-utils.R
@@ -1,16 +1,3 @@
-##' <description>
-##'
-##' <details>
-##' @title 
-##' @param x 
-##' @param y 
-##' @param length 
-##' @param width 
-##' @param startAngle 
-##' @param sweepLength 
-##' @return 
-##' @author tengfei
-##' @export
 qglyphSector <- function(x,y,length,width,startAngle,sweepLength){
   len <- (length+width)*2
   r <- length+width
@@ -24,25 +11,11 @@ qglyphSector <- function(x,y,length,width,startAngle,sweepLength){
   r <- r-width
   x0 <- x-r
   y0 <- y-r
-  ## movex <- x+r*cos((startAngle+sweepLength)/180*pi)
-  ## movey <- y+r*sin((startAngle+sweepLength)/180*pi)
-  ##glyph$lineTo(movex,movey)
   glyph$arcTo(x0,y0,2*r,2*r,-(startAngle+sweepLength),sweepLength)
   glyph$closeSubpath()
   glyph
 }
-##' <description>
-##'
-##' <details>
-##' @title 
-##' @param x 
-##' @param y 
-##' @param r 
-##' @param startAngle 
-##' @param sweepLength 
-##' @return 
-##' @author tengfei
-##' @export
+
 qglyphArc <- function(x,y,r,startAngle,sweepLength){
   x0 <- x-r
   y0 <- y-r
@@ -55,35 +28,13 @@ qglyphArc <- function(x,y,r,startAngle,sweepLength){
   glyph
 }
 
-##' <description>
-##'
-##' <details>
-##' @title 
-##' @param startpoint 
-##' @param controlpoint 
-##' @param endpoint 
-##' @return 
-##' @author tengfei
-##' @export
 qglyphQuadCurve <- function(startpoint,controlpoint,endpoint){
   glyph <- Qt$QPainterPath()
   glyph$moveTo(startpoint[1],startpoint[2])
   glyph$quadTo(controlpoint[1],controlpoint[2],endpoint[1],endpoint[2])
   glyph
 }
-##' <description>
-##'
-##' <details>
-##' @title 
-##' @param startpoint1 
-##' @param controlpoint1 
-##' @param endpoint1 
-##' @param startpoint2 
-##' @param controlpoint2 
-##' @param endpoint2 
-##' @return 
-##' @author tengfei
-##' @export
+
 qglyphCurveBundle <- function(startpoint1,controlpoint1,endpoint1,
                               startpoint2,controlpoint2,endpoint2){
   glyph <- Qt$QPainterPath()
@@ -96,31 +47,26 @@ qglyphCurveBundle <- function(startpoint1,controlpoint1,endpoint1,
   glyph
 }
 
-## library(qtpaint)
-## s <- qscene()
-## paths <- list(qglyphQuadCurve(c(-120,-120),c(0,100),c(140,140)),
-##              c(-120,-120),c(100,0),c(140,140))
-##  myfun <- function(layer,painter){
-##     qantialias(painter) <- FALSE
-##     qlineWidth(painter) <- 2
-##     qdrawPath(painter,paths,stroke=rainbow(length(paths)))  
-##  }
-##  l <- qlayer(s,myfun,limits=qrect(-140,-140,140,140))
-##  v <- qplotView(s)
-##  v$show()
+bluered_pal <- function(){
+  function(x){
+    x <- cscale(x,rescale_pal())
+    x <- x-0.5
+    idx <- x>=0
+    col.red <- cscale(x[idx],rescale_pal())
+    col.red <- rgb(col.red,0,0)
+    col.blue <- cscale(abs(x[!idx]),rescale_pal())
+    col.blue <- rgb(0,0,col.blue)
+    col <- vector("numeric",length(x))
+    col[idx] <- col.red
+    col[!idx] <- col.blue
+    col
+  }
+}
 
-## s <- qscene()
-## paths <- list(qglyphCurveBundle(c(-120,-120),c(-85,60),c(-120,140),
-##               c(-50,140),c(-85,60),c(-50,-120)))
-## paths <- list(qglyphCurveBundle(c(-50,-120),c(-85,60),c(-120,140),
-##               c(-50,140),c(-85,60),c(-120,-120)))
-##  myfun <- function(layer,painter){
-## ##    qantialias(painter) <- FALSE
-##     qfillColor(painter) <- 'red'
-## ##    qlineWidth(painter) <- 2
-##     qdrawPath(painter,paths,stroke=rainbow(length(paths)))
-##  }
-##  l <- qlayer(s,myfun,limits=qrect(-140,-140,140,140))
-##  v <- qplotView(s)
-##  v$show()
-
+blackred_pal <- function(){
+  function(x){
+    x <- cscale(x,rescale_pal())
+    x <- 1-x
+    col <- rgb(x,0,0)
+  }
+}
