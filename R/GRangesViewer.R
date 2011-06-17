1ccda79ab7d1687c393aa7ae168a780bb8d5b167
Jason Crowley <crowley.jason.s@gmail.com>
2011-06-17 16:10:08 -0700

    add simple GRanges viewer GUI (still needs view embedded)
    A simple GRanges viewer with a search bar that had autocompletion
    and can query gene IDs, stored in genesymbol.rda.

---
+++ R/GRangesViewer.R
@@ -0,0 +1,214 @@
+#library(qtbase)
+#library(GenomicRanges)
+
+#load("genesymbol.rda")
+
+#gr1 <-
+#  GRanges(seqnames =
+#          Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
+#          ranges =
+#          IRanges(1:10, end = 7:16, names = head(letters, 10)),
+#          strand =
+#          Rle(strand(c("-", "+", "*", "+", "-")),
+#              c(1, 2, 2, 3, 2)),
+#          score = 1:10,
+#          GC = seq(1, 0, length=10))
+
+qsetClass("GRangesViewer", Qt$QWidget, function(gr, ref = NULL, parent = NULL)
+{
+  super(parent)
+
+  this$setMinimumSize(550,350)
+  this$setWindowTitle("Simple Genome Viewer")
+
+  #this$view <- WIDGET FROM TENGFEI
+  this$tv <- Qt$QTableView()
+  this$proxy <- Qt$QSortFilterProxyModel()
+  tv$sortingEnabled <- TRUE
+
+
+  this$sb <- SearchBar(gr, ref)
+  this$submit <- Qt$QPushButton("Submit")
+
+  # initiate parse when user clicks submit (will eventually trigger table
+  # filtering)
+  qconnect(submit, "clicked", function() {
+    sb$parseSearchString(sb$text, gr, ref)
+  })
+
+  # update table when search range updates
+  qconnect(sb, "rangeChanged", function() {
+    ret <- sb$getSearchRange()
+    if(!is.null(ret)) {
+      filterTable(gr, ret)
+    }
+  })
+  
+  sbLyt <- Qt$QHBoxLayout()
+  sbLyt$addWidget(sb)
+  sbLyt$addWidget(submit)
+
+  windowLyt <- Qt$QVBoxLayout()
+  #windowLyt$addWidget(view)
+  windowLyt$addWidget(tv)
+  windowLyt$addLayout(sbLyt)
+  setLayout(windowLyt)
+
+})
+
+qsetMethod("filterTable", GRangesViewer, function(gr, grInterval) {
+  grFilter <- subsetByOverlaps(gr, grInterval)
+
+  filterModel <- qdataFrameModel(as.data.frame(grFilter))
+  proxy$setSourceModel(filterModel)
+  tv$setModel(proxy)
+  
+})
+
+qsetClass("SearchBar", Qt$QLineEdit, function(gr, ref = NULL, parent = NULL)
+{
+  super(parent)
+  this$gr <- gr; this$ref <- ref
+
+  names <- levels(seqnames(gr))
+  metadata <- unique(unlist(sapply(values(ref),as.character)))
+  namesRef <- NULL
+  metadataRef <- NULL
+  if(!is.null(ref)) {
+    namesRef <- levels(seqnames(ref))
+    metadataRef <- unique(unlist(sapply(values(ref),as.character)))
+  }
+  compVec <- sort(unique(c(names,metadata,namesRef,metadataRef)))
+  comp <- Qt$QCompleter(compVec)
+  this$setCompleter(comp)
+
+  # initialize the GRanges object containing the search range
+  searchRange <- NULL
+
+  # parse the text and update GRanges object when return pressed
+  qconnect(this, "returnPressed", function() parseSearchString(this$text, gr, ref))
+})
+
+qsetSignal("rangeChanged", SearchBar)
+
+qsetMethod("getSearchRange", SearchBar, function() {
+  this$searchRange
+})
+
+qsetMethod("setSearchRange", SearchBar, function(newRange) {
+  this$searchRange <- newRange
+})
+
+qsetMethod("parseSearchString", SearchBar, function(text, gr, ref = NULL) {
+  colon <- grepl(":",text,fixed=TRUE)
+  minus <- grepl("-",text,fixed=TRUE)
+  plus <- grepl("+",text,fixed=TRUE)
+
+  if(colon & minus) {  # seqname and interval specified
+    # obtain seqname, start, end
+    colonIdx <- regexpr(":",text,fixed=TRUE)
+    minusIdx <- regexpr("-",text,fixed=TRUE)
+    name <- substr(text,1,colonIdx-1)
+    start <- as.integer(substr(text,colonIdx+1,minusIdx-1))
+    end <- as.integer(substr(text,minusIdx+1,nchar(text)))
+
+    # return GRanges object with relevant sequence
+    this$searchRange <- GRanges(seqnames =
+                     Rle(name, 1),
+                     ranges =
+                       IRanges(start = start, end = end),
+                   )
+
+    this$rangeChanged()
+           
+  } else if(colon & plus) {  # seqname and center/radius specified
+    # obtain seqname, start, end
+    colonIdx <- regexpr(":",text,fixed=TRUE)
+    plusIdx <- regexpr("+",text,fixed=TRUE)
+    name <- substr(text,1,colonIdx-1)
+    center <- as.integer(substr(text,colonIdx+1,plusIdx-1))
+    radius <- as.integer(substr(text,plusIdx+1,nchar(text)))
+    start <- center - radius
+    end <- center + radius
+
+    # return GRanges object with relevant sequence
+    this$searchRange <- GRanges(seqnames =
+                     Rle(name, 1),
+                     ranges =
+                       IRanges(start = start, end = end),
+                   )
+
+    this$rangeChanged()
+    
+  } else {
+    grNames <- levels(seqnames(gr))
+    in.names <- grep(text,grNames)
+    metaData <- unlist(sapply(values(gr),as.character))
+    in.metadata <- grep(text,metaData)
+    if(!is.null(ref)) {
+      namesReference <- levels(seqnames(ref))
+      in.namesRef <- grep(text,namesReference)
+      metaDataReference <- unlist(sapply(values(ref),as.character))
+      in.metadataRef <- grep(text,metaDataReference)
+    }
+    if(length(in.names) > 0) {
+      grSubset <- gr[seqnames(gr) == text]
+      minLoc <- min(start(grSubset))
+      maxLoc <- max(end(grSubset))
+
+      # return GRanges object with relevant sequence
+      this$searchRange <- GRanges(seqnames =
+                       Rle(text, 1),
+                       ranges =
+                         IRanges(start = minLoc, end = maxLoc),
+                     )
+
+      this$rangeChanged()
+    } else if (length(in.metadataRef) > 0 & (!is.null(ref))) {
+      matchRows <- in.metadataRef %% dim(values(ref))[1]
+      matchRows[matchRows == 0] <- dim(values(ref))[1]
+      
+      # return GRanges object with matching rows in reference set
+      refSubset <- ref[matchRows]
+      this$searchRange <- refSubset
+
+      this$rangeChanged()
+
+      # change text in search bar to match intervals being displayed
+      setText(base::paste(GenomicRanges::seqnames(refSubset),":",
+                          IRanges::start(refSubset),
+                          "-",IRanges::end(refSubset),collapse="; ",sep=""))
+    } else if (length(in.namesRef) > 0) {
+      refSubset <- ref[seqnames(ref) == text]
+      minLoc <- min(start(refSubset))
+      maxLoc <- max(end(refSubset))
+
+      # return GRanges object with relevant sequence
+      this$searchRange <- GRanges(seqnames =
+                       Rle(text, 1),
+                       ranges =
+                         IRanges(start = minLoc, end = maxLoc),
+                     )
+
+      this$rangeChanged()      
+    } else if (length(in.metadata) > 0) {
+      matchRows <- in.metadata %% dim(values(gr))[1]
+      matchRows[matchRows == 0] <- dim(values(gr))[1]
+      
+      # return GRanges object with matching rows in reference set
+      grSubset <- gr[matchRows]
+      this$searchRange <- grSubset
+
+      this$rangeChanged()
+
+      # change text in search bar to match intervals being displayed
+      setText(base::paste(GenomicRanges::seqnames(grSubset),":",
+                          IRanges::start(grSubset),"-",
+                          IRanges::end(grSubset),collapse="; ",sep=""))
+    } else {
+      setText("Error: search string not recognized")
+      this$searchRange <- NULL
+    }
+  }
+
+})
