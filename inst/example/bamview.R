d4f90512cfd769023b83fb4c9e0ddf6bca80364c
tengfei <tengfei@tengfei-T510.(none)>
2011-07-22 17:29:29 -0700

    remove examples

--- inst/example/bamview.R
+++
@@ -1,5 +0,0 @@
-require(visnab)
-data(bamtest)
-library(IRanges)
-visenv$themes[["default"]][["bg.col"]] <- "white"
-visplot(bamtest,lower=1)
--- inst/example/reference.R
+++
@@ -1,6 +0,0 @@
-require(visnab)
-require(BSgenome.Hsapiens.UCSC.hg19)
-test <- SeqView(Hsapiens)
-refs <- visplot(test,"chr1")
-
-
